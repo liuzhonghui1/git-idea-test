7fa437aca87ab40c7f72b54a3047e8aae9ff79d5
liuzhonghui1 <liuzhonghui_yaya@aliyun.com>
2021-11-12 14:52:18 +0800

    second commit

--- src/main/java/com/mide/test1/FirstClass.java
+++ src/main/java/com/mide/test1/FirstClass.java
@@ -3,5 +3,6 @@ package com.mide.test1;
 public class FirstClass {
     public static void main(String[] args) {
         System.out.println("first class");
+        System.out.println("first class 222");
     }
 }
