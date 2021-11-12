38159ba24064a49e0376a266a6d13ece7a427f79
liuzhonghui1 <49111312+liuzhonghui1@users.noreply.github.com>
2021-11-12 17:25:55 +0800

    Update FirstClass.java
    github edit

--- src/main/java/com/mide/test1/FirstClass.java
+++ src/main/java/com/mide/test1/FirstClass.java
@@ -8,5 +8,6 @@ public class FirstClass {
         System.out.println("first class 333-hot fix");
         System.out.println("first class 666 master");
         System.out.println("first class 777 master push github");
+        System.out.println("first class 888 github edit");
     }
 }
