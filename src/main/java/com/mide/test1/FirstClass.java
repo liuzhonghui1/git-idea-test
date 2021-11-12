91877a32b983fe736dd4a611e35daee3445bf03b
liuzhonghui1 <liuzhonghui_yaya@aliyun.com>
2021-11-12 15:12:55 +0800

    foruth marster commit

--- src/main/java/com/mide/test1/FirstClass.java
+++ src/main/java/com/mide/test1/FirstClass.java
@@ -4,5 +4,6 @@ public class FirstClass {
     public static void main(String[] args) {
         System.out.println("first class");
         System.out.println("first class 222");
+        System.out.println("first class 444 master");
     }
 }
